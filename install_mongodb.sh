15720f1c0d1093f748df34558bee33f4ed059338
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-01-11 12:37:26 +0530

    roboshop tf ec2 applications

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,10 +1,9 @@
-
 echo "[mongodb-org-7.0]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/9/mongodb-org/7.0/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://pgp.mongodb.com/server-7.0.asc"> /etc/yum.repos.d/mongo.repo
+gpgkey=https://pgp.mongodb.com/server-7.0.asc" > /etc/yum.repos.d/mongo.repo
 
 dnf install mongodb-org -y
 systemctl enable mongod
