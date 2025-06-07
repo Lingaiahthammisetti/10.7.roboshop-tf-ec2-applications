408ac57cf2e8aa8ae28cdcce6ff1c0b3883e7f0d
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-06-07 21:51:15 +0530

    roboshop tf ec2 applications

--- variables.tf
+++ variables.tf
@@ -3,12 +3,12 @@ variable "instances" {
     default = {
         mongodb  = "t3.small"
         redis    = "t3.micro"
-        mysql    = "t3.small"
+        mysql    = "t3.medium"
         rabbitmq = "t3.micro"
         catalogue= "t3.micro"
         user     = "t3.micro"
         cart     = "t3.micro"
-        shipping = "t3.small"
+        shipping = "t3.medium"
         payment  = "t3.micro"
         dispatch = "t3.micro"
        #frontend = "t3.micro"
@@ -16,12 +16,12 @@ variable "instances" {
     }
 }
 variable "allow_all" {
-   type = string
-   default = "sg-06b1b57b365846051"
+    type = string
+    default = "sg-09c7c70bd56f0d58b"
 }
 variable "zone_id" {
-    default = "Z0660538W3DX3HZU59ZR"
+    default ="Z07608533HHBQHW47XU3F"
 }
 variable "domain_name" {
-  default = "lingaiah.online"
+    default = "lingaiah.online"
 }
